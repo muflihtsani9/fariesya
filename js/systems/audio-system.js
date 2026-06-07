4655d284e5f16d8c75a3bd5b6d0922a2295d6718
muflihtsani9 <muflihtsani9@gmail.com>
2026-06-07 22:31:45 +0700

    first commit

--- js/systems/audio-system.js
+++ js/systems/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'my darling',
-    artist:    'Skyline',
+    title:     'Raindance',
+    artist:    'Dave',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 50,       // seconds — where playback begins
